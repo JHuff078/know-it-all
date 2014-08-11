335abeaa1664eb24f5435ae73bda85b72a86ff8d
Julian Huff <jhuff078@gmail.com>
2014-08-11 15:52:39 -0400

    Back to one file…

--- src/main.c
+++ src/main.c
@@ -1,8 +1,98 @@
-#include <pebble.h>
-#include "time_layer.h"
-#include "weather_layer.h"    
+#include <pebble.h> 
+
+#define KEY_TEMPERATURE 0
+#define KEY_CONDITIONS 1
 
 Window *s_main_window;
+
+TextLayer *s_time_layer;
+TextLayer *s_weather_layer;
+
+GFont *s_time_font;
+GFont *s_weather_font;
+
+void update_time() {
+    //Get a tm structure
+    time_t temp = time(NULL);
+    struct tm *time_tick = localtime(&temp);
+    
+    //Create a long-lived buffer
+    char buffer[] = "00:00";
+    
+    //Write the current hours and minutes into the buffer
+    if(clock_is_24h_style() == true) {
+        //Use 24h format
+        strftime(buffer, sizeof("00:00"), "%H:%M", time_tick);
+    } else {
+        //User 12H format
+        strftime(buffer, sizeof("00:00"), "%I:%M", time_tick);
+    }
+    
+    //Display this time on the TextLayer
+    text_layer_set_text(s_time_layer, buffer);
+}
+
+void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+    
+    //Get weather update every 30 mintues
+    if (tick_time->tm_min % 30 == 0) {
+        //Begin dictionary
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
+        
+        //Add a key-value pair
+        dict_write_uint8(iter, 0, 0);
+        
+        //Send the message
+        app_message_outbox_send();
+    }
+}
+
+void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    //Store incoming information
+    char temperature_buffer[8];
+    char conditions_buffer[32];
+    char weather_layer_buffer[32];
+    
+    //Read first item
+    Tuple *t = dict_read_first(iterator);
+    
+    //For all items
+    while (t != NULL) {
+        //Which key was received?
+        switch (t->key) {
+            case KEY_TEMPERATURE:
+                snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int) t->value->int32);
+                break;
+            case KEY_CONDITIONS:
+                snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+                break;
+            default:
+                APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int) t->key);
+                break;
+        }
+        
+        //Look for next item
+        t = dict_read_next(iterator);
+    }
+    
+    //Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+}
+
+void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
 
 void main_window_load(Window *window) {
     //Set the windows background color
@@ -45,7 +135,7 @@ void main_window_unload(Window *window) {
     fonts_unload_custom_font(s_weather_font);
 }
 
-static void init() {
+void init() {
     //Create the main window and assign to the pointer
     s_main_window = window_create();
     
@@ -74,7 +164,7 @@ static void init() {
     app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
-static void deinit() {
+void deinit() {
     //Destroy the window
     window_destroy(s_main_window);
 }
--- src/time_layer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-TextLayer *s_time_layer;
-
-GFont *s_time_font;
-
-void update_time();
-
-void tick_handler(struct tm *tick_time, TimeUnits units_changed);
